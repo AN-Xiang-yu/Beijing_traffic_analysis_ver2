c0efd23c877226ec89df952e5f6a430fa2b7e6be
AN <409227823@qq.com>
2022-10-24 22:10:15 +0200

    mse à jour

--- best_route.py
+++ best_route.py
@@ -44,8 +44,8 @@ def searchPoint(long, lat, file, Pointtype):
                 # We can calculate its linear distance in latitude and longitude, and then display
 
                 # Calculate the distance between M and the input point
-                tempoDistance = ((long-float(longM))*5000)**3 + \
-                    ((lat-float(latM))*5000)**3
+                tempoDistance = ((long-float(longM))*10000)**2 + \
+                    ((lat-float(latM))*10000)**2
                 if tempoDistance < distance:
                     # tempoDistance is smaller than dis, then update distance, and update idPoint.
                     distance = tempoDistance
